daf85116afe0dce77572d1d32dcb6f57d074d830
Nicolas <nicolasae2108@gmail.com>
2023-07-21 10:24:02 -0500

    Creación de variables globales para conversión de xlsx a csv

---
+++ Proyecto Pago Seguridad Social/Scripts/xlsx_to_csv_converter.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+import csv
+
+# Global Variables
+month = "Junio"
+# Get the path of the directory where the convert_to_csv.py script is located
+script_directory = os.path.dirname(os.path.abspath(__file__))
+# Get the absolute path to the xlsx "files" directory by joining it with the script directory
+xlsx_directory = os.path.abspath(os.path.join(script_directory, "..", "Files", "xlsx", month))
+csv_directory = os.path.abspath(os.path.join(script_directory, "..", "Files", "csv", month))

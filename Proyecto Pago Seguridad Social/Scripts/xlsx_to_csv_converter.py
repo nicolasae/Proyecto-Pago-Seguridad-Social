bc7b267b16447688569c1d756679dbdaf3dfaf59
Nicolas <nicolasae2108@gmail.com>
2023-07-21 10:30:36 -0500

    Agrega función convert_all_files_to_csv() y la utiliza para convertir archivos .xlsx a archivos .csv

--- Proyecto Pago Seguridad Social/Scripts/xlsx_to_csv_converter.py
+++ Proyecto Pago Seguridad Social/Scripts/xlsx_to_csv_converter.py
@@ -22,4 +22,36 @@ def create_folder_if_not_exists(folder_path):
     else:
         print(f"Folder '{folder_path}' already exists.")
 
-create_folder_if_not_exists(csv_directory)
+def convert_all_files_to_csv( xlsx_directory, csv_directory):
+    # Verify if the month directory exist
+    create_folder_if_not_exists(csv_directory)
+
+    # Get the list of files in the folder
+    files_list = os.listdir(xlsx_directory)
+
+    # Iterate over each file in the folder
+    for file in files_list:
+        # Check if it's a valid file 
+        if file.endswith('.xlsx'): 
+            # Full path of the file
+            file_path = os.path.join(xlsx_directory, file)
+            
+            # Read the file into a DataFrame 
+            data = pd.read_excel(file_path)
+
+            # Generate the new file name
+            file_name, file_extension = os.path.splitext(file)
+            new_file_name = file_name + "_converted.csv"
+
+            # Full path of the new file
+            new_file_path = os.path.join(csv_directory, new_file_name)
+
+            # # Convert and save as CSV
+            data.to_csv(new_file_path, index=False)
+
+            # # Print success message for each converted file
+            print(f'The file {file} has been successfully converted to CSV as {new_file_name}.')
+
+
+# Call Functions
+convert_all_files_to_csv(xlsx_directory, csv_directory)

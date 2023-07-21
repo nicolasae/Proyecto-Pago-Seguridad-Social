386140658945d93d7700a26afdee21f146f4bb1f
Nicolas <nicolasae2108@gmail.com>
2023-07-21 11:06:32 -0500

    Llama a la función delete_columns_by_words para eliminar columnas en el archivo principal

--- Proyecto Pago Seguridad Social/Scripts/patronales.py
+++ Proyecto Pago Seguridad Social/Scripts/patronales.py
@@ -10,6 +10,34 @@ words_to_delete = ["UNIDAD 3", "UNIDAD 4", "UNIDAD 5","DSCTO ASMETSALUD PARA SAN
 # Path to the CSV file
 csv_file_path = os.path.join(csv_directory,file_name)
 
+def search_word_in_csv(csv_file_path, search_word):
+    with open(csv_file_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for index_row, row in enumerate(reader):
+            for index_col, value in enumerate(row):
+                if value == search_word:
+                    return index_col
+    return None
+
+def delete_column_by_index(file_path, index_to_delete):
+    # Read the entire content of the original CSV file
+    with open(file_path, 'r', newline='') as csvfile:
+        rows = list(csv.reader(csvfile))
+
+    # Validate if the provided index is valid
+    num_columns = len(rows[0])
+    if index_to_delete < 0 or index_to_delete >= num_columns:
+        raise ValueError("Invalid index for the column to delete.")
+
+    # Remove the desired column from each row
+    for row in rows:
+        del row[index_to_delete]
+
+    # Overwrite the original file with the updated content
+    with open(file_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(rows)
+
 def delete_columns_by_words(file_path, words_to_delete):
     # Get the indices of columns to delete based on the words found
     columns_to_delete = []

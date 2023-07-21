70cb89b9a2d65df120f21f9a964a14f41579f7cd
Nicolas <nicolasae2108@gmail.com>
2023-07-21 14:31:43 -0500

    Añadir credenciales de bd local

--- Proyecto Pago Seguridad Social/django_project_pss/django_project_pss/settings.py
+++ Proyecto Pago Seguridad Social/django_project_pss/django_project_pss/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'django_project_pss.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bd_seguridad_social',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Puedes cambiarlo al host de tu servidor MySQL si es diferente.
+        'PORT': '',          # Deja este campo en blanco para usar el puerto predeterminado (generalmente 3306).
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

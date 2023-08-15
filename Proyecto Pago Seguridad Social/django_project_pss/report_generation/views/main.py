bebaa9a8f839f88bba615a7d1424f15d43c66e3a
Nicolas <nicolasae2108@gmail.com>
2023-08-15 11:19:01 -0500

    Añade boton de descargar de documentos por fecha

--- Proyecto Pago Seguridad Social/django_project_pss/report_generation/views/main.py
+++ Proyecto Pago Seguridad Social/django_project_pss/report_generation/views/main.py
@@ -1,6 +1,10 @@
+import os
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.conf import settings
 from document_upload.models import *
+import zipfile
+
 from .report_planilla import *
 from .report_patronales import *
 from .report_deducciones import *
@@ -17,21 +21,14 @@ def download_view(request):
         if 'btn_resumen_patronales' in request.POST:
             return create_report_patronales(request, selected_year, selected_month)
         
-        if 'btn_resumen_patronales_temporales' in request.POST:
-            return create_report_temporales(request, selected_year, selected_month)
-        
-        if 'btn_resumen_patronales_permanentes' in request.POST:
-            return create_report_permanentes(request, selected_year, selected_month)           
-        
         if 'btn_resumen_deducciones' in request.POST:
-            return create_report_deducciones(request, selected_year, selected_month)           
-        
-        if 'btn_revision' in request.POST:
-            return create_revision(request, selected_year, selected_month)           
+            return create_report_deducciones(request, selected_year, selected_month)                                
         
         if 'btn_consolidado' in request.POST:
-            return create_report_consolidado(request, selected_year, selected_month)           
+            return create_report_consolidado(request, selected_year, selected_month)
         
+        if 'btn_descargar' in request.POST:
+            return download_documents(request, selected_year, selected_month)             
     
     return render(request, 'reports.html')
 
@@ -93,3 +90,38 @@ def create_report_consolidado(request, year, month):
             'message':f'No hay información disponible para realizar un informe consolidado del periodo: {date}.',
         }        
         return render(request, 'reports.html', context)
+    
+def download_documents(request,year,month):
+    folder_path = os.path.join(settings.MEDIA_ROOT, 'xlsx', year, month)
+    
+    context = {
+        'show_alert': True,
+        'alert_type':"danger",
+        'message':f'No hay documentos disponibles para descargar del periodo: {year}/{month}.',
+    }        
+    if not os.path.exists(folder_path):
+        return render(request, 'reports.html', context)
+    
+    # Get file list by directory 
+    files_in_folder = os.listdir(folder_path)
+
+    # Check if the folder is empty 
+    if not files_in_folder:     
+        return render(request, 'reports.html', context)
+
+    # Create a ZIP file in memory
+    response = HttpResponse(content_type='application/zip')
+    response['Content-Disposition'] = 'attachment; filename=carpeta_descargada.zip'
+
+    memory_zip = zipfile.ZipFile(response, 'w', zipfile.ZIP_DEFLATED)
+
+    for foldername, subfolders, filenames in os.walk(folder_path):
+        for filename in filenames:
+            filepath = os.path.join(foldername, filename)
+            arcname = os.path.relpath(filepath, folder_path)
+            memory_zip.write(filepath, arcname)
+
+    # Close the ZIP file
+    memory_zip.close()
+
+    return response

d6da2342c4293004c41ca87300005f5de02778b4
Nicolas <nicolasae2108@gmail.com>
2023-09-04 15:35:29 -0500

    Se agrega autenticación en las vistas para subir documentos y entidades

--- Proyecto Pago Seguridad Social/django_project_pss/document_upload/urls.py
+++ Proyecto Pago Seguridad Social/django_project_pss/document_upload/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import main
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('subir/', main.upload_documents, name='subir'),
-    path('subir/entidades/', main.upload_data_entidades, name='subir_entidades'),
+    path('subir/', login_required(main.upload_documents), name='subir'),
+    path('subir/entidades/', login_required(main.upload_data_entidades), name='subir_entidades'),
     path('listar/entidades/', main.render_list_entidades, name='listar_entidades'),
     path('listar/plantillas/', main.render_list_plantillas, name='listar_plantilas'),
 ]
